b3cf37a5c7b042622d5b0d8eeb846efb046994e7
fernndinho <102922290+fernndinho@users.noreply.github.com>
2023-12-30 17:15:34 -0300

    feat: added a greeter class

---
+++ src/main/java/me/fernndinho/tests/Greeter.java
@@ -0,0 +1,9 @@
+package me.fernndinho.tests;
+
+public class Greeter {
+
+    public static String getHelloMessage(String name) {
+        return "Hello " + name + "!";
+    }
+
+}
--- src/main/java/me/fernndinho/tests/Main.java
+++ src/main/java/me/fernndinho/tests/Main.java
@@ -2,6 +2,7 @@ package me.fernndinho.tests;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hi, i am a test");
+        String message = Greeter.getHelloMessage("Fer");
+        System.out.println(message);
     }
 }
